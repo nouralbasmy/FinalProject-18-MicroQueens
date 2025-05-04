d75307143be839e88b18d12f17eecf324f2a35c1
Sara Khaled <sarasallam21802@gmail.com>
2025-05-04 23:33:46 +0300

    simulating PushNotification

--- notification/src/main/java/com/example/notification/strategy/PushNotificationStrategy.java
+++ notification/src/main/java/com/example/notification/strategy/PushNotificationStrategy.java
@@ -9,8 +9,15 @@ public class PushNotificationStrategy implements NotificationStrategy{
 
     @Override
     public void send(Notification notification) {
+        if (notification.getDeviceToken() == null || notification.getDeviceToken().isEmpty()) {
+            throw new IllegalArgumentException("Device token is missing.");
+        }
 
-        System.out.println("Push was sent to token "+notification.getDeviceToken()+ " " +notification.getMessage());
+        // Simulating the push notification
+        System.out.println("Simulating push notification to device token: " + notification.getDeviceToken());
+        System.out.println("Message: " + notification.getMessage());
     }
+    
 
 }
+
