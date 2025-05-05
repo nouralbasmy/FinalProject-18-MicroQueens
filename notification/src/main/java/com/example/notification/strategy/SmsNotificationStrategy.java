38e2e15a8e6bbfd24a964e6eef31f799b7fbdd65
Sara Khaled <sarasallam21802@gmail.com>
2025-05-05 21:29:53 +0300

    added simulation for sms

--- notification/src/main/java/com/example/notification/strategy/SmsNotificationStrategy.java
+++ notification/src/main/java/com/example/notification/strategy/SmsNotificationStrategy.java
@@ -1,5 +1,10 @@
 package com.example.notification.strategy;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 import com.example.notification.model.Notification;
@@ -9,8 +14,27 @@ public class SmsNotificationStrategy implements NotificationStrategy {
 
     @Override
     public void send(Notification notification) {
+        if (notification.getPhone() == null || notification.getPhone().isBlank())
+           throw new IllegalArgumentException("phone required");
+
+        //simulation since everything is very costly :(
+           System.out.printf(
+            """
+            ─── Simulated SMS ───────────────────────────────────────
+            To      : %s
+            Order   : %d
+            Restaurant : %d
+            Message : %s
+            ─────────────────────────────────────────────────────────
+            """,
+            notification.getPhone(),
+            notification.getOrderId(),
+            notification.getRestaurantId(),
+            notification.getMessage()
+        );
+
+       
     
-        System.out.println("Sending SMS: "+ notification.getPhone()  + notification.getMessage());
     }
 
 }
