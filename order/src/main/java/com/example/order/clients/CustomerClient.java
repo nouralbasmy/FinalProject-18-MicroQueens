67a5d6ca7c0474f856f096f543b53a201e551cb6
nouralbasmy <58092657+nouralbasmy@users.noreply.github.com>
2025-05-18 02:50:55 +0300

    Fixed environment variables issue in Dockerfile & Deployment file

--- order/src/main/java/com/example/order/clients/CustomerClient.java
+++ order/src/main/java/com/example/order/clients/CustomerClient.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.Map;
 
-@FeignClient(name = "customer-service", url = "http://localhost:8094/customer")
+//@FeignClient(name = "customer-service", url = "http://localhost:8094/customer")
+@FeignClient(name = "customer-service", url = "${customer.url}")
 public interface CustomerClient {
     @GetMapping("/validateToken")
     Map<String, String> decodeToken(@RequestHeader("Authorization") String authHeader);
